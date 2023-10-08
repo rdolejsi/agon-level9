81723eb16c4f4bc0e0b62a2a082fdd5c6e2df2d6
Roman Dolejsi <rdolejsi@gmail.com>
2023-10-08 12:20:07 +0200

    Agon Light port: screen dimensions detected upon startup, printing adjusted accordingly; file name prompts code unification

--- Agon/agon.c
+++ Agon/agon.c
@@ -31,14 +31,29 @@
 char text_buffer[TEXTBUFFER_SIZE + 1];
 int text_buffer_pointer = 0;
 
-int Column = 0;
-#define SCREENWIDTH 76
+uint8_t column = 0;
+static uint16_t screen_width;
+static uint16_t screen_height;
+static uint8_t screen_columns;
+static uint8_t screen_rows;
+static uint8_t screen_colors;
+
+/**
+ * Loads the current graphic mode details for future use.
+ */
+void os_graphics_inquiry() {
+    screen_width = getsysvar_scrwidth();
+    screen_height = getsysvar_scrheight();
+    screen_columns = getsysvar_scrCols();
+    screen_rows = getsysvar_scrRows();
+    screen_colors = getsysvar_scrColours();
+}
 
 void os_printchar(char c) {
     if (c == '\r') {
         os_flush();
         putchar('\n');
-        Column = 0;
+        column = 0;
     } else if (isprint(c) != 0) {
         if (text_buffer_pointer >= TEXTBUFFER_SIZE) {
             os_flush();
@@ -101,7 +116,7 @@ void os_flush(void) {
     }
     *(text_buffer + text_buffer_pointer) = ' ';
     int ptr = 0;
-    while (text_buffer_pointer + Column > SCREENWIDTH) {
+    while (text_buffer_pointer + column > screen_columns - 1) {
         register int space = ptr;
         register int last_space = space;
         register bool searching = TRUE;
@@ -109,10 +124,10 @@ void os_flush(void) {
             while (text_buffer[space] != ' ') {
                 space++;
             }
-            if (space - ptr + Column > SCREENWIDTH) {
+            if (space - ptr + column > screen_columns - 1) {
                 space = last_space;
                 printf("%.*s\n", space - ptr, text_buffer + ptr);
-                Column = 0;
+                column = 0;
                 space++;
                 if (text_buffer[space] == ' ') {
                     space++;
@@ -128,21 +143,25 @@ void os_flush(void) {
     }
     if (text_buffer_pointer > 0) {
         printf("%.*s", text_buffer_pointer, text_buffer + ptr);
-        Column += text_buffer_pointer;
+        column += text_buffer_pointer;
     }
     text_buffer_pointer = 0;
 }
 
-L9BOOL os_save_file(L9BYTE *Ptr, int Bytes) {
+FILE *os_prompt_and_open_file(const char *prompt, const char *mode) {
     char name[256];
     os_flush();
-    printf("Save file: ");
+    printf("%s", prompt);
     fgets(name, 256, stdin);
     char *nl = strchr(name, '\n');
     if (nl) {
         *nl = 0;
     }
-    FILE *f = fopen(name, "wb");
+    return fopen(name, mode);
+}
+
+L9BOOL os_save_file(L9BYTE *Ptr, int Bytes) {
+    FILE *f = os_prompt_and_open_file("Save file: ", "wb");
     if (!f) {
         return FALSE;
     }
@@ -151,20 +170,12 @@ L9BOOL os_save_file(L9BYTE *Ptr, int Bytes) {
     return TRUE;
 }
 
-L9BOOL os_load_file(L9BYTE *Ptr, int *Bytes, int Max) {
-    char name[256];
-    os_flush();
-    printf("Load file: ");
-    fgets(name, 256, stdin);
-    char *nl = strchr(name, '\n');
-    if (nl) {
-        *nl = 0;
-    }
-    FILE *f = fopen(name, "rb");
+L9BOOL os_load_file(L9BYTE *pointer, int *bytes, int max) {
+    FILE *f = os_prompt_and_open_file("Load file: ", "rb");
     if (!f) {
         return FALSE;
     }
-    *Bytes = fread(Ptr, 1, Max, f);
+    *bytes = fread(pointer, 1, max, f);
     fclose(f);
     return TRUE;
 }
@@ -193,6 +204,19 @@ void os_set_filenumber(char *new_name, int size, int n) {
     }
 }
 
+FILE *os_open_script_file(void) {
+    return os_prompt_and_open_file("Script file: ", "rt");
+}
+
+L9BOOL os_find_file(char *new_name) {
+    FILE *f = fopen(new_name, "rb");
+    if (f != NULL) {
+        fclose(f);
+        return TRUE;
+    }
+    return FALSE;
+}
+
 void os_graphics(int mode) {
 }
 
@@ -211,29 +235,12 @@ void os_fill(int x, int y, int colour1, int colour2) {
 void os_show_bitmap(int pic, int x, int y) {
 }
 
-FILE *os_open_script_file(void) {
-    char name[256];
-    os_flush();
-    printf("Script file: ");
-    fgets(name, 256, stdin);
-    char *nl = strchr(name, '\n');
-    if (nl) {
-        *nl = 0;
-    }
-    return fopen(name, "rt");
-}
-
-L9BOOL os_find_file(char *new_name) {
-    FILE *f = fopen(new_name, "rb");
-    if (f != NULL) {
-        fclose(f);
-        return TRUE;
-    }
-    return FALSE;
-}
-
 int main(int argc, char **argv) {
-    printf("Level 9 Interpreter\n\n");
+    os_graphics_inquiry();
+    printf(
+            "Level 9 Interpreter, mode %ix%i (%ix%i chars), %i colors\n",
+            screen_width, screen_height, screen_columns, screen_rows, screen_colors
+    );
     if (argc != 2) {
         printf("Use: %s <game_file>\n", argv[0]);
         return 0;
@@ -243,7 +250,6 @@ int main(int argc, char **argv) {
         return 0;
     }
     while (RunGame());
-    printf("Stopping game now\n");
     StopGame();
     FreeMemory();
     return 0;
